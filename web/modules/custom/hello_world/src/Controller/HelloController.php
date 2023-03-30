02de6667353919dd5d2df4b9af57adf54f6ca415
Anastasiya Kazakova <ctasik@gmail.com>
2023-03-30 19:37:51 -0400

    class 3

--- web/modules/custom/hello_world/src/Controller/HelloController.php
+++ web/modules/custom/hello_world/src/Controller/HelloController.php
@@ -3,6 +3,9 @@
 namespace Drupal\hello_world\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\node\Entity\Node;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
 
 /**
  * Class HelloController
@@ -12,13 +15,31 @@ use Drupal\Core\Controller\ControllerBase;
  * Hello World method
  */
 class HelloController extends ControllerBase {
-  public function hello($name = NULL) {
-      if ($name){
-          $output = $this->t("Hello @name", ['@name' => $name]);
-        }
-      else{
-          $output = $this->t("Hello World");
-        }
+  public function hello($name = NULL, $nid = NULL) {
+    $output = $this->t('Hello :name!', [':name' => $name]);
+    if(!is_null($nid) && is_numeric($nid)){
+      $node = Node::load($nid);
+      
+      if($node) {
+        //ksm($node);
+        //ksm($node->getTitle());
+        //ksm($node->title->value);
+        //ksm($node->toLink());
+    
+
+        //$title = $node->getTitle();
+        //$link = $node->toLink()->toString();
+        $url = Url::fromRoute('entity.node.canonical', ['node' => $nid]);
+        ksm($url);
+        $link = Link::fromTextAndUrl($node->getTitle(), $url);
+        ksm($link);
+
+        $output = $this->t('Hello @name, The title is @title', ['@name' => $name, '@title' => $link->toString()]);
+      }
+      else {
+        $output = $this->t('Hello :name! This is not a node with ID @nid', ['@name' => $name, '@nid' => $nid]);
+      }
+    }
       return [
           '#type' => 'markup',
           '#markup' => $output,
