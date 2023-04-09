375c08ed282eaf2b27f985b0b49ffa342f855b69
Anastasiya Kazakova <ctasik@gmail.com>
2023-04-09 16:53:46 -0400

    Youtube video formatter homework

---
+++ web/modules/custom/youtube_video_formatter/src/Plugin/Field/FieldFormatter/YoutubeFieldFormatter.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Drupal\youtube_video_formatter\Plugin\Field\FieldFormatter;
+
+use Drupal\Core\Field\FieldItemListInterface;
+use Drupal\Core\Field\FormatterBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Plugin implementation of the 'Youtube Video' formatter.
+ *
+ * @FieldFormatter(
+ *   id = "youtube_video_formatter",
+ *   label = @Translation("Youtube Video"),
+ *   field_types = {
+ *     "string"
+ *   }
+ * )
+ */
+class YoutubeFieldFormatter extends FormatterBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultSettings() {
+    return [
+      'width' => '',
+      'height' => '',
+    ] + parent::defaultSettings();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+
+    $form['width'] = [
+      '#type' => 'number',
+      '#title' => 'Video Width',
+      '#default_value' => $this->getSetting('width'),
+      '#required' => TRUE,
+    ];
+
+    $form['height'] = [
+      '#type' => 'number',
+      '#title' => 'Video Height',
+      '#default_value' => $this->getSetting('height'),
+      '#required' => TRUE,
+    ];
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsSummary() {
+    $summary[] = 'The width is ' . $this->getSetting('width');
+    $summary[] = 'The height is ' . $this->getSetting('height');
+    return $summary;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function viewElements(FieldItemListInterface $items, $langcode) {
+    $element = [];
+
+    foreach ($items as $delta => $item) {
+      $elements[$delta] = [
+        '#theme' => 'youtube_video',
+        '#youtube_id' => $item->value,
+        '#width' => $this->getSetting('width'),
+        '#height' => $this->getSetting('height'),
+      ];
+    }
+
+    return $elements;
+  }
+
+}
