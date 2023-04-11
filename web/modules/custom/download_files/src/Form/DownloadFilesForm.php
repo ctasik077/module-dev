4088b53d336a076ff9c8294f655312bf8815d3c8
Anastasiya Kazakova <ctasik@gmail.com>
2023-04-11 17:31:36 -0400

    Lesson 5 homework

--- web/modules/custom/download_files/src/Form/DownloadFilesForm.php
+++ web/modules/custom/download_files/src/Form/DownloadFilesForm.php
@@ -1,28 +1,25 @@
 <?php
-
 namespace Drupal\download_files\Form;
-
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\file\Entity\File;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 /**
  * Provides a Download files form.
  */
 class DownloadFilesForm extends FormBase {
-
   /**
    * {@inheritdoc}
    */
   public function getFormId() {
     return 'download_files_form';
   }
-
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-
     $form['file'] = [
       '#type' => 'select',
       '#title' => $this->t('Select a file'),
@@ -30,9 +27,16 @@ class DownloadFilesForm extends FormBase {
       '#options' => $this->getFiles(),
     ];
 
+    $form['pass_phrase'] = [
+      '#type' => 'email',
+      '#title' => $this->t('Email'),
+      '#description' => $this->t('Enter your email address to retrieve the file.'),
+    ];
+
     $form['actions'] = [
       '#type' => 'actions',
     ];
+
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Download'),
@@ -40,22 +44,31 @@ class DownloadFilesForm extends FormBase {
 
     return $form;
   }
-
   /**
    * Get array with list of files.
    */
   public function getFiles() {
+    // Get files using database abstraction layer.
     $result = \Drupal::database()
       ->select('file_managed', 'f')
       ->fields('f', ['filename', 'uri'])
       ->condition('f.status', 1)
       ->execute()
       ->fetchAll();
-
     $files = [];
     foreach ($result as $file) {
       $files[$file->uri] = $file->filename;
     }
+
+    // Get files using entity queries.
+    // $fids = \Drupal::entityQuery('file')
+    //   ->condition('status', 1)
+    //   ->execute();
+    // $items = File::loadMultiple($fids);
+    // $files = [];
+    // foreach ($items as $item) {
+    //   $files[$item->getFileUri()] = $item->getFilename();
+    // }
     return $files;
   }
 
@@ -63,17 +76,22 @@ class DownloadFilesForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    if (mb_strlen($form_state->getValue('message')) < 10) {
-      $form_state->setErrorByName('message', $this->t('Message should be at least 10 characters.'));
+    parent::validateForm($form, $form_state);
+    $pass_phrase = $form_state->getValue('pass_phrase');
+    if (!strpos($pass_phrase, 'evolvingweb.com')) {
+      $form_state->setErrorByName('pass_phrase', $this->t('Invalid email.'));
     }
   }
 
   /**
    * {@inheritdoc}
    */
+
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->messenger()->addStatus($this->t('The message has been sent.'));
-    $form_state->setRedirect('<front>');
+    $uri = $form_state->getValue('file');
+    $response = new BinaryFileResponse($uri);
+    $response->setContentDisposition('attachment');
+    $form_state->setResponse($response);
   }
 
 }
